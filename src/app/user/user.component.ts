e930aaed7062ba16fa51085b0d5f66e0660ede28
EdwardLee <edwardlee098@gmail.com>
2024-08-25 00:48:13 +0800

    使用Signal input 作為輸入資料，但是會無法從內部修改(onSelectUser)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,15 +8,27 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input() avatar!: string;
-  // @Input() name!: string;
-
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // 1. use signal input
+  // avatar = input<string>();
+  // name = input<string>();
+  // 2. add required
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  // use signal computed
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {
+    // 使用signal時，內部唯獨屬性，不能被賦值，Property 'set' does not exist on type 'InputSignal<string>'
+    // 所以此種情況建議使用Input decorator
+    // this.avatar.set();
+  }
 }
