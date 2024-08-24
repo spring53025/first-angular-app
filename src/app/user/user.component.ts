bda29b5e91435d381ba9afab4afd1be5d314dcc5
EdwardLee <edwardlee098@gmail.com>
2024-08-24 23:50:39 +0800

    refactor: Improve user component code structure and readability

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,29 +11,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // A. property
-  // selectedUser = DUMMY_USERS[randomIndex];
-
-  // B. use signal to create a reactive property
   selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  // A. zone.js getter function
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
-
-  // B. use computed signal to get the image path
   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   onSelectUser() {
-    // click event handler
-    // 每次點擊時，隨機選擇一個用戶
     console.log('Clicked!');
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-    // A. this.selectedUser = DUMMY_USERS[randomIndex];
-
-    // B. set the signal value
     this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
