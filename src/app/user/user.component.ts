c7321732ae3a217f937afcd67fb8329e54606d69
EdwardLee <edwardlee098@gmail.com>
2024-08-26 22:25:15 +0800

    1. 使用output()函数获取用户输入的字符串，效果等同於@Output()，且這個output方法不是一個signal。 2. 使用@Output，建立EventEmitter時，指定泛型的型別，這樣會比較適當。

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +11,10 @@ export class UserComponent {
   @Input({ required: true}) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>(); //指定泛型的型別，這樣會比較適當
+  // select = output<string>(); //不是一個signal，而是一個OutputEmitter
+  // 比較新的方法，但建議如果使用@Input，才同時使用@Output
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
@@ -19,5 +22,7 @@ export class UserComponent {
 
   onSelectUser() {
     this.select.emit(this.id);
+    // this.select.emit(2); //如果沒指定泛型的型別，這裡輸入int，則不會有錯誤提示
+    //但是另一邊的onSelectUser(id: string) 方法會有問題
   }
 }
