f41fc93e544958f4438d6432e69b47e4e8d7abd6
EdwardLee <edwardlee098@gmail.com>
2024-08-26 23:12:44 +0800

    1. 建立tasks commponent，在app component 接收 id, 建立一個 selectedUser getter 回傳 user 的資料(使用 find method)，在app.component.html 使用 [name] 綁定 selectedUser.name 顯示使用者名稱，最後在 tasks.component.html 顯示 name，其中name 是在tasks.component.ts 使用 @Input 取得。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,7 +18,16 @@ export class AppComponent {
 
   users = DUMMY_USERS;
 
+  selectUserId = 'u1';
+
+  // 建立一個getter，用來取得選擇的user
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectUserId)!; // 使用驚嘆號，表示一定會找到
+  }
+
   onSelectUser(id: string) {
-    console.log('Select user with id', id);
+    // console.log('Select user with id', id);
+    // 這邊的id是從user.component.ts中的emit傳過來的
+    this.selectUserId = id;
   }
 }
