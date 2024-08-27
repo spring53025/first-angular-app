2514cc2400e110abeb8a3178dad47c7884744572
EdwardLee <edwardlee098@gmail.com>
2024-08-27 22:05:26 +0800

    使用@if 判斷是否需要顯示，而不是一開始就顯示，@else 如果不符合條件就顯示其他內容，其中條件可以放入變數或是函式。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
 
   users = DUMMY_USERS;
-
-  selectUserId = 'u1';
+  selectedUserId?: string;
 
   // 建立一個getter，用來取得選擇的user
   get selectedUser() {
-    return this.users.find((user) => user.id === this.selectUserId); // 使用驚嘆號，表示一定會找到
+    return this.users.find((user) => user.id === this.selectedUserId); // 使用驚嘆號，表示一定會找到
   }
 
   onSelectUser(id: string) {
-    this.selectUserId = id;
+    this.selectedUserId = id;
   }
 }
