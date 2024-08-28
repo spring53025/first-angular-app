e83db3e2a44e407df6ea1b704a2b832480516e35
EdwardLee <edwardlee098@gmail.com>
2024-08-28 16:06:02 +0800

    使用舊版ngFor,ngIf指令產生DOM資料，與@for,@if指令不同，ngFor,ngIf指令會產生新的DOM元素，並且會在DOM元素上加上ng-前綴的屬性，這些屬性是Angular用來控制DOM元素的指令。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -8,7 +10,7 @@ import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
